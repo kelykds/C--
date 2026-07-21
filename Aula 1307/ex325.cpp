a838a89fc45d28676423e8b9c09838679a81fd5b
Kely <citizenkds@gmail.com>
2026-07-21 19:18:24 +0000

    Refatorar cálculo de tempos e remover vetor desnecessário

--- Aula 1307/ex325.cpp
+++ Aula 1307/ex325.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> // min()
 
 using namespace std;
 
 int main() {
     int npessoas; 
     cin >> npessoas;
-    vector<int> tempos, tempmax;
-    for(int i = 0; i < npessoas; i++) {
-        int tempo;
-        cin >> tempo;
-        tempos.push_back(tempo);
-    } // aqui vamos ter os tempos bonitinhos
+
+    if (npessoas == 0) {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    vector<int> tempos(npessoas);
     
     for(int i = 0; i < npessoas; i++) {
-        tempmax.push_back(tempos[i] + 10);
-    } // pega quando cada um saiu da esteira
+        cin >> tempos[i];
+    
+    } // aqui vamos ter os tempos bonitinhos
+    
+    int tempotot = 0; 
+    
+    // do primeiro até o penultimo
+    for(int i = 0; i < npessoas - 1; i++) {
+        int dif = tempos[i+1] - tempos[i];
+        // adiciona o menor valor entre 10 e a dif de tempo
+        tempotot += min(10, dif);
+    }
+    // a última pessoa sempre garante 10s de escada ligada
+    tempotot += 10;
 
-    // tem que verificar se coincidem...
-
+    cout << tempotot; // é até simples, eu que não consegui raciocinar
 
     return 0;
 }
