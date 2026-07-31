1726097af7b1dcce194d027d74065ea6f5479195
Kely <citizenkds@gmail.com>
2026-07-31 17:11:20 -0300

    Create f1.cpp with main function and struct definition
    Initialize f1.cpp with basic structure and input handling.

---
+++ Neps/f1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct {
+    int id;
+    int pontuação;
+}
+
+int main() {
+
+    int g, p;
+    cin >> g >> p;
+    // se botar em vector, presta? não complica dps?... struct..
+    vector<vector<int>> chegada;
+
+    for (int i = 0; i < g; i++) {
+        for (int j = 0; j < p; i++) {
+             
+        }
+    }
+
+    return 0;
+}
