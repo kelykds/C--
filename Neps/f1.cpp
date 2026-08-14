5b1b80ab4adaf78c0ca6ab03008a3c9401524ebc
Kely <citizenkds@gmail.com>
2026-08-14 16:05:59 -0300

    Refactor input storage to use posicoes vector
    Replace 'chegada' with 'posicoes' vector for input storage and initialize total_pts vector.

--- Neps/f1.cpp
+++ Neps/f1.cpp
@@ -12,12 +12,25 @@ int main() {
     int g, p;
     cin >> g >> p;
     // se botar em vector, presta? não complica dps?... struct..
-    vector<vector<int>> chegada;
+    vector<vector<int>> posicoes(g, vector<int>(p));
 
     for (int i = 0; i < g; i++) {
         for (int j = 0; j < p; i++) {
-             
+             cin >> posicoes[i][j];
         }
+    }
+
+    int s;
+
+
+    vector<int> total_pts(p, 0);
+
+    for(int i = 0; i < g; i++) {
+        for (int j = 0; j < p; j++) {
+            int pos = posicoes[i][j];
+
+            i
+        } 
     }
 
     return 0;
